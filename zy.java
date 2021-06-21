06e2869eddc84f99d272f60b5e4df127d0432ee1
zwxzy <82144496+zwxzy@users.noreply.github.com>
2021-06-21 15:00:28 +0800

    Create zy.java

---
+++ zy.java
@@ -0,0 +1,10 @@
+ public class cheng {    //乘法
+     int a = 1;
+      long b = 1000000;
+     float c = 0.000001f;
+    double d = 99.99;
+           public void chengfa() {
+                double cheng = a * b * c * d;
+                 System.out.println("乘法运算结果是：" + cheng);
+   　　　　 }
+ }
